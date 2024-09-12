3dd7cfd8fa303aca404664996d8f1251282858f4
Alyaman Massarani <alyaman.maasarani@gmail.com>
2024-09-12 11:39:12 -0400

    Added support for CUDA at the expense of limiting to x86_64-linux

--- flake.nix
+++ flake.nix
@@ -7,37 +7,69 @@
       nixpkgs,
     }:
     let
-      supportedSystems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
     in
     {
-      devShells = forAllSystems (system: {
-        default = pkgs.${system}.mkShellNoCC {
-          packages = with pkgs.${system}; [
-            jupyter-all
-            python312Packages.numpy
-            python312Packages.pandas
-            python312Packages.matplotlib
-            python312Packages.scikit-learn
-            python312Packages.scipy
+      devShells."x86_64-linux" = {
+        default =
+          (pkgs.buildFHSUserEnv {
+            name = "cuda-env";
+            targetPkgs =
+              pkgs: with pkgs; [
+                jupyter-all
+                python312Packages.numpy
+                python312Packages.pandas
+                python312Packages.matplotlib
+                python312Packages.scikit-learn
+                python312Packages.scipy
 
-            # Required for matplotlib to display with GTK4Cairo backend
-            librsvg
-            python312Packages.pygobject3
-            python312Packages.pycairo
-            gtk4
-          ];
-          shellHook = ''
-            export MPLBACKEND="GTK4Cairo"
-          '';
-        };
-      });
-      formatter = forAllSystems (system: pkgs.${system}.nixfmt-rfc-style);
+                # Required for matplotlib to display with GTK4Cairo backend
+                librsvg
+                python312Packages.pygobject3
+                python312Packages.pycairo
+                gtk4
+
+                # Required for CUDA. Taken from "https://nixos.org/manual/nixpkgs/stable/#sec-fhs-environments"
+                git
+                gitRepo
+                gnupg
+                autoconf
+                curl
+                procps
+                gnumake
+                util-linux
+                m4
+                gperf
+                unzip
+                cudatoolkit
+                linuxPackages.nvidia_x11
+                libGLU
+                libGL
+                xorg.libXi
+                xorg.libXmu
+                freeglut
+                xorg.libXext
+                xorg.libX11
+                xorg.libXv
+                xorg.libXrandr
+                zlib
+                ncurses5
+                stdenv.cc
+                binutils
+              ];
+            multiPkgs = pkgs: with pkgs; [ zlib ];
+            runScript = "bash";
+            profile = ''
+              export CUDA_PATH=${pkgs.cudatoolkit}
+              # export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib
+              export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+              export EXTRA_CCFLAGS="-I/usr/include"
+            '';
+          }).env;
+      };
+      formatter."x86_64-linux" = pkgs.nixfmt-rfc-style;
     };
 }
